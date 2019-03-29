60d7d2f29d366ade3f1bb0535b19efead2781d5e
toyboot4e <toyboot4e@gmail.com>
2019-03-29 16:13:46 +0900

    fixed project name from lox to loxrs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use std::env; // args
 use std::io; // Result
 use std::vec::Vec;
-extern crate lox;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     match args.len() {
         0 | 1 => {
-            lox::run_repl();
+            loxrs::run_repl();
         }
         n if n >= 2 => {
-            lox::run_file(&args[1]);
+            loxrs::run_file(&args[1]);
         }
         _ => {
             panic!("");
