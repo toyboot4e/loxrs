37fe42cec351974222295fe5c7283ec34182f8a2
toyboot4e <toyboot4e@gmail.com>
2019-09-03 20:24:31 +0900

    fix comments

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 #![warn(rust_2018_idioms)]
+
 mod abs;
 mod interpreter;
 mod walk;
@@ -12,7 +13,7 @@ use crate::interpreter::Interpreter;
 use crate::walk::{parser::Parser, scanner::Scanner};
 
 use std::fs;
-use std::io::{self, Write}; // flush()
+use std::io::{self, BufRead, BufWriter, Write}; // flush()
 
 // TODO: returning error
 pub fn run_file(path: &str) {
@@ -24,34 +25,42 @@ pub fn run_file(path: &str) {
         Ok(s) => s,
     };
     let (tokens, scan_errors) = Scanner::new(&source).scan();
-    self::print_all("scan errors:", &scan_errors);
-    self::print_all("tokens:", &tokens);
+    self::print_all("====== scan errors =====", &scan_errors);
+    self::print_all("====== tokens =====", &tokens);
     let (mut stmts, parse_errors) = Parser::new(&tokens).parse();
-    self::print_all("parse errors:", &parse_errors);
+    self::print_all("===== parse errors =====", &parse_errors);
     self::interpret(&mut stmts);
 }
 
-// TODO: more generic code
 fn print_all<T>(description: &str, items: &[T])
 where
     T: std::fmt::Debug,
 {
-    println!("{}", description);
+    let out = io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    write!(out, "{}", description).unwrap();
     for i in items {
-        println!("  {:?}", i);
+        writeln!(out, "{:?}", i).unwrap();
     }
-    println!("");
+    writeln!(out).unwrap();
 }
 
 pub fn run_repl() {
     println!("Entered Lox REPL");
     let prompt_str = "> ";
+
     let mut line = String::new();
+    // We can use [LineWriter](https://doc.rust-lang.org/std/io/struct.LineWriter.html) instead
+    // to automate flushing
+    let out = io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    let handle = io::stdin();
+    let mut handle = handle.lock();
     loop {
         print!("{}", prompt_str);
-        io::stdout().flush().expect("error when flushing stdout");
+        out.flush().expect("error when flushing stdout");
         line.clear();
-        io::stdin()
+        handle
             .read_line(&mut line)
             .expect("error when reading stdin");
         match line.trim_end() {
@@ -65,12 +74,16 @@ pub fn run_repl() {
 
 pub fn interpret(stmts: &mut [Stmt]) {
     let mut interpreter = Interpreter::new();
-    println!("interruption:");
-    for stmt in stmts {
-        // println!("  stmt: {:?}", stmt);
-        if let Err(why) = interpreter.interpret(stmt) {
-            println!("RUNTINE ERROR: {:?}", why);
-            break;
+    println!("====== interuption =====");
+    match stmts
+        .iter()
+        .map(|x| interpreter.interpret(x))
+        .find(|x| x.is_err())
+    {
+        Some(err) => {
+            println!("====== runtime errors =====");
+            println!("{:?}", err);
         }
+        None => {}
     }
 }
